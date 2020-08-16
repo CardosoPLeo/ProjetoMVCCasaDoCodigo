d0d526e05ea3aff5366a843b8f93eeaecf90d1f9
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-08-16 20:05:49 -0300

    Criacao do da classe de ApplicationContext para usarmos o Entity FrameWork

---
+++ ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        //substituição do método par criar o o
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+        }
+    }
+}
